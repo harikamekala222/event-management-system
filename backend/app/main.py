51b1fd53a9400a4393a914116ef5fd9855f6ecfd
Ubuntu <ubuntu@ip-172-31-29-62.ap-south-1.compute.internal>
2026-09-16 10:24:18 +0000

    1st

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://3.110.64.127"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
